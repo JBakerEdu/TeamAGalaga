72f099901e0c27c7f2f3bdae8cf352f6363dafa5
Trinidad Dena <tdena1@my.westga.edu>
2024-11-27 21:27:00 -0500

    Added Scoreboard Functionality

---
+++ Galaga/View/HighScorePage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Galaga.View
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class HighScorePage : Page
+    {
+        HighScoreViewModel HighScoreViewModel;
+
+        public HighScorePage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void SortSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var viewModel = this.HighScoreViewModel;
+            viewModel.SortOrder = ((ComboBoxItem)((ComboBox)sender).SelectedItem).Content.ToString();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            this.HighScoreViewModel = new HighScoreViewModel((int)e.Parameter);  
+            this.DataContext = this.HighScoreViewModel;
+        }
+    }
+}
--- Galaga/View/UITextManager.cs
+++ Galaga/View/UITextManager.cs
@@ -1,109 +1,127 @@
-﻿using System;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
+﻿using Galaga.Model;
 using Galaga.View;
+using System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml;
+using System.Threading.Tasks;
 
-namespace Galaga.Model
+public class UiTextManager
 {
+    public event Action<bool> GameEnded;
+
+    private readonly Canvas canvas;
+    private readonly GameManager gameManager;
+    private TextBlock scoreTextBlock;
+    private TextBlock playerLivesTextBlock;
+    private int score;
+
     /// <summary>
-    /// This is the text Manager over the UI , so it is the manager over the text that appears on the canvas
+    /// Marks when game is over
     /// </summary>
-    public class UiTextManager
+    public bool GameOver { get; private set; }
+    private TextBlock gameOverTextBlock;
+
+    /// <summary>
+    /// This is the constructor for the UiTextManager and will set and initialize the correct data onto the canvas. 
+    /// </summary>
+    /// <param name="canvas">the canvas to add the text onto</param>
+    /// <param name="playerLives">the life count for the player ship</param>
+
+    public UiTextManager(Canvas canvas, int playerLives, GameManager gameManager)
     {
-        private readonly Canvas canvas;
-        private readonly GameManager gameManager;
-        private TextBlock scoreTextBlock;
-        private TextBlock playerLivesTextBlock;
-        private int score;
-        /// <summary>
-        /// Marks when game is over
-        /// </summary>
-        public bool GameOver { get; private set; }
-        private TextBlock gameOverTextBlock;
+        this.canvas = canvas;
+        this.gameManager = gameManager;
+        this.GameOver = false;
+        this.initializeScoreGame();
+        this.initializePlayerLives(playerLives);
 
-        /// <summary>
-        /// This is the constructor for the UiTextManager and will set and initialize the correct data onto the canvas. 
-        /// </summary>
-        /// <param name="canvas">the canvas to add the text onto</param>
-        /// <param name="playerLives">the life count for the player ship</param>
-        public UiTextManager(Canvas canvas, int playerLives, GameManager gameManager)
+        EnemyManager.OnGameEnd += isWin =>
         {
-            this.canvas = canvas;
-            this.gameManager = gameManager;
-            this.GameOver = false;
-            this.initializeScoreGame();
-            this.initializePlayerLives(playerLives);
-        }
-
-
-        private void initializeScoreGame()
-        {
-            this.scoreTextBlock = new TextBlock
+            if (isWin)
             {
-                Text = "Score: 0",
+                this.EndGame(isWin);
+            }
+            else
+            {
+                this.EndGame(isWin);
+            }
+        };
+
+    }
+
+    private void initializeScoreGame()
+    {
+        this.scoreTextBlock = new TextBlock
+        {
+            Text = "Score: 0",
+            FontSize = 24,
+            Foreground = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.White),
+            Margin = new Thickness(this.canvas.Width - this.canvas.Width / 4, 10, 0, 0)
+        };
+        this.canvas.Children.Add(this.scoreTextBlock);
+    }
+
+    private void initializePlayerLives(int playerLives)
+    {
+        this.playerLivesTextBlock = new TextBlock
+        {
+            Text = $"Lives: {playerLives}",
+            FontSize = 24,
+            Foreground = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.White),
+            Margin = new Thickness(10, 10, 0, 0)
+        };
+        this.canvas.Children.Add(this.playerLivesTextBlock);
+    }
+
+    /// <summary>
+    /// this updates the score on screen witht the enemy ships point value added to current score
+    /// </summary>
+    /// <param name="enemy"> the enemy ship so it can get the correct point value</param>
+
+    public void UpdateScore(EnemyShip enemy)
+    {
+        this.score += enemy.Score;
+        this.scoreTextBlock.Text = $"Score: {this.score}";
+    }
+
+    /// <summary>
+    /// this updates the players lives left removing lives as the player gets hit
+    /// </summary>
+    /// <param name="playerLives">the number of lives left</param>
+
+    public void UpdatePlayerLives(int playerLives)
+    {
+        playerLives = Math.Max(0, playerLives);
+        this.playerLivesTextBlock.Text = $"Lives: {playerLives}";
+    }
+
+
+
+    /// <summary>
+    /// will set the end game out put based on the boolean value of if the player wins or not
+    /// </summary>
+    /// <param name="win">if the player wins or looses</param>
+    public async void EndGame(bool win)
+    {
+        if (!this.GameOver)
+        {
+            String gameOverText = win ? "You Win!!!" : "You Lose!!!";
+            this.GameOver = true;
+            this.gameManager.BonusShipSpawn(false);
+            this.gameOverTextBlock = new TextBlock
+            {
+                Text = gameOverText,
                 FontSize = 24,
                 Foreground = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.White),
-                Margin = new Thickness(this.canvas.Width - this.canvas.Width / 4, 10, 0, 0)
+                Margin = new Thickness(10, this.canvas.Height - 40, 0, 0)
             };
-            this.canvas.Children.Add(this.scoreTextBlock);
-        }
+            this.canvas.Children.Add(this.gameOverTextBlock);
+            AudioManager.PlayGameOver();
 
-        private void initializePlayerLives(int playerLives)
-        {
-            this.playerLivesTextBlock = new TextBlock
-            {
-                Text = $"Lives: {playerLives}",
-                FontSize = 24,
-                Foreground = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.White),
-                Margin = new Thickness(10, 10, 0, 0)
-            };
-            this.canvas.Children.Add(this.playerLivesTextBlock);
-        }
+            await Task.Delay(3000);
 
-        /// <summary>
-        /// this updates the score on screen witht the enemy ships point value added to current score
-        /// </summary>
-        /// <param name="enemy"> the enemy ship so it can get the correct point value</param>
-        public void UpdateScore(EnemyShip enemy)
-        {
-            this.score += enemy.Score;
-            this.scoreTextBlock.Text = $"Score: {this.score}";
-        }
-
-        /// <summary>
-        /// this updates the players lives left removing lives as the player gets hit
-        /// </summary>
-        /// <param name="playerLives">the number of lives left</param>
-        public void UpdatePlayerLives(int playerLives)
-        {
-            if (playerLives <= 0)
-            {
-                playerLives = 0;
-            }
-            this.playerLivesTextBlock.Text = $"Lives: {playerLives}";
-        }
-
-        /// <summary>
-        /// will set the end game out put based on the boolean value of if the player wins or not
-        /// </summary>
-        /// <param name="win">if the player wins or looses</param>
-        public void EndGame(bool win)
-        {
-            if (!this.GameOver)
-            {
-                String gameOverText = win ? "You Win!!!" : "You Loose!!!";
-                this.GameOver = true;
-                this.gameManager.BonusShipSpawn(false);
-                this.gameOverTextBlock = new TextBlock
-                {
-                    Text = gameOverText,
-                    FontSize = 24,
-                    Foreground = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.White),
-                    Margin = new Thickness(10, this.canvas.Height - 40, 0, 0)
-                };
-                this.canvas.Children.Add(this.gameOverTextBlock);
-                AudioManager.PlayGameOver();
-            }
+            (Window.Current.Content as Frame)?.Navigate(typeof(HighScorePage), this.score);
         }
     }
 }
