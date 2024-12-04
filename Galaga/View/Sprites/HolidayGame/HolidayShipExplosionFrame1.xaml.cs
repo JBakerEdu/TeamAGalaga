479d03be7ce95c404e625d7f1db0746e9a990ae7
Jacob <jb00645@my.westga.edu>
2024-12-04 00:28:33 -0500

    Started Implementing Holiday Version (WOW)
    Cretaed a file for new holiday sprites to go for the holiday version of the game, and added a couple sprites in with santa player  and snowman explosions.

---
+++ Galaga/View/Sprites/HolidayGame/HolidayShipExplosionFrame1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Galaga.View.Sprites
+{
+    /// <summary>
+    /// Defines HolidayShipExplosionFrame1 from which all sprites inherit.
+    /// </summary>
+    public partial class HolidayShipExplosionFrame1
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HolidayShipExplosionFrame1"/> class.
+        /// </summary>
+        public HolidayShipExplosionFrame1()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Renders sprite at the specified (x,y) location in relation
+        /// to the top, left part of the canvas.
+        /// </summary>
+        /// <param name="x">x location</param>
+        /// <param name="y">y location</param>
+        public new void RenderAt(double x, double y)
+        {
+            Canvas.SetLeft(this, x);
+            Canvas.SetTop(this, y);
+        }
+    }
+
+
+    
+}
